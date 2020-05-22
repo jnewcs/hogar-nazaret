5a59feee50f9c73810c4dfc63dc3acaafe592810
John Newcomb <jxnewcomb@godaddy.com>
2020-05-22 16:37:12 -0700

    Fixing links with query string parameters

--- js/language_changer.js
+++ js/language_changer.js
@@ -2,10 +2,10 @@ if (navigator && navigator.language || navigator.browserLanguage) {
   var language = navigator.language || navigator.browserLanguage;
 
   if (language === 'es' || language.indexOf('es-') !== -1) {
-    window.location.replace(window.location.href + 'es');
+    window.location.replace(window.location.origin + '/es');
   }
 
   if (language === 'en' || language.indexOf('en-') !== -1) {
-    window.location.replace(window.location.href + 'en');
+    window.location.replace(window.location.origin + '/en');
   }
 }
