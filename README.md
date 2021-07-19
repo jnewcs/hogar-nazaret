1e38f637e8cf3dc28d76ab89056277a785ec22ba
jnewcs <jnewcs@users.noreply.github.com>
2021-07-19 10:18:21 +0000

    jekyll build from Action a547091fae549759f0a030afdf5aebf96cf2bd0a

---
+++ README.md
@@ -0,0 +1,112 @@
+# Repo for hogar-nazaret
+The repo for the website for the Hogar Albergue para Niños Jesús de Nazaret. It is built on [Jekyll](https://jekyllrb.com), an open source project based on Ruby.
+
+Having our site built on Jekyll, allows us to generate a static website which can be hosted on Github Pages, reducing our cost and allowing us to host the code on Github.
+
+# Development Setup
+The following instructions are meant for people who want to build the site locally (on your own machine) for development purposes. If you don't want to build the site locally, you can always modify any file in the `master` branch and it will build through GitHub Pages. This is, however, riskier since you cannot verify the changes before they become live.
+
+Before you can build the site locally, you need a few things setup:
+
+1. A GitHub account
+    - You can create an account by going to [www.github.com](https://www.github.com) and signing up
+    - You also need to be a collaborator for this repo. Please contact j.newcomb@outlook.com to request access
+
+2. A way to interact with git on your computer
+    - You can do this through one of two ways:
+        - Github Desktop
+            - If you haven't used git much before, this is probably the right choice. It gives you [a visual interface for git](https://desktop.github.com)
+        - Using git in your command line
+            - [Link to Installation Guide](https://help.github.com/en/github/getting-started-with-github/set-up-git)
+            - After you install git, you probably want to use SSH so you don't have to reauthenticate every single time you commit something. To do this, you need to attach a SSH Key Pair to your GitHub account:
+                - If you haven't done so already, you should [generate a personal ssh key pair (https://help.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh) and register it with your GitHub account.
+                - I usually take all the default values (just keep hitting enter until it completes) when creating the SSH key pair.
+                - Once done, login to [www.github.com](https://www.github.com) and navigate to your account settings. Under SSH Keys, click on the Add button and paste the contents of `~/.ssh/id_rsa.pub` into the key area. More detailed instructions can be found [here](https://help.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account)
+
+3. Ruby installed
+    - Jekyll requires a Ruby version installed and the Jekyll developer site has [great documentation to install Ruby on your computer](https://jekyllrb.com/docs/installation/) for various operating systems.
+    - If you can run `ruby -v` and see a valid result, you have a version of Ruby installed on your computer and you should be able to proceed with the rest of the setup.
+    - Jekyll needs Ruby 2.5 or higher, so you might have to install a higher version of Ruby to begin.
+    - As a heads up, you will probably need `sudo` access to install Ruby.
+
+4. Jekyll installed
+    - Once Ruby has been installed, you can install Jekyll following the [documentation from Jekyll](https://jekyllrb.com/docs/installation/)
+    - The basic command to install Jekyll should look something like this: `gem install --user-install jekyll`
+
+5. Install Bundler
+    - Since Jekyll is written in Ruby, it utilizes [gems](https://jekyllrb.com/docs/ruby-101/#gems) to add functionality to the sites. A lot of Jekyll repos, including this one, have a `Gemfile` - a list of gems required for the site. The purpose of the Bundler is to install gems from a `Gemfile`
+    - Once Ruby is installed, you should be able to run `gem install bundler` to install the Bundler.
+
+
+### Clone Repo
+
+Once those items ^ are setup, you should clone this repo (using SSH) into a folder on your computer. For my workflow, I put the repo in the `~/personal_repos` folder, but you can place the repo wherever you can. You can run the following (you might have the make the folder first if it does not exist):
+
+```
+cd ~/personal_repos
+
+git clone git@github.com:jnewcs/hogar-nazaret.git
+```
+
+### Run Bundler
+Once the repo has been cloned locally, you should navigate into the repo and run bundler to install the [gems](https://jekyllrb.com/docs/ruby-101/#gems) specified in this repo:
+
+```
+cd ~/personal_repos/hogar-nazaret  # You don't need this if you are already in the hogar-nazaret repo
+
+bundle install
+```
+
+### Build & view the site locally
+You are now ready to preview content locally. Jekyll is a static site generator so every time we change a file, it generates a new build of the site and stores it within the `_site` folder locally. This mimics the building process that GitHub Pages does for us to publish the site to production at https://hogarjesusdenazaret.org. This is the basic command you can run to build the site locally:
+
+```
+cd ~/personal_repos/hogar-nazaret  # You don't need this if you are already in the hogar-nazaret repo
+
+bundle exec jekyll serve
+```
+
+This command should output something like this:
+
+```
+Configuration file: /Users/jxnewcomb/personal_repos/hogar-nazaret/_config.yml
+            Source: /Users/jxnewcomb/personal_repos/hogar-nazaret
+       Destination: /Users/jxnewcomb/personal_repos/hogar-nazaret/_site
+ Incremental build: disabled. Enable with --incremental
+      Generating...
+       Jekyll Feed: Generating feed for noticias
+       Jekyll Feed: Generating feed for upcoming_events
+       Jekyll Feed: Generating feed for proximos_eventos
+       Jekyll Feed: Generating feed for posts
+   GitHub Metadata: No GitHub API authentication could be found. Some fields may be missing or have incorrect data.
+                    done in 8.791 seconds.
+ Auto-regeneration: enabled for '/Users/jxnewcomb/personal_repos/hogar-nazaret'
+    Server address: http://127.0.0.1:4000
+  Server running... press ctrl-c to stop.
+```
+
+If it works properly, you now can preview the site at http://127.0.0.1:4000. If the `serve` command is still running and you make a change, you can see that change reflected locally at http://127.0.0.1:4000 once the build finishes. Every time you change something, it might take `7+` seconds for the build to finish and for you to be able to see the change in the browser.
+
+### Preview content locally to mimic "Staging" environment
+We currently use [Forestry.io](https://forestry.io/) as our content management system (CMS). Within our CMS, we can preview content before commiting the changes to the `master` branch which will change https://hogarjesusdenazaret.org. We call this our "Staging" environment. If you want to view the "Staging" environment locally, you can run this command:
+
+```
+cd ~/personal_repos/hogar-nazaret  # You don't need this if you are already in the hogar-nazaret repo
+
+bundle exec jekyll serve --drafts --future --unpublished --port 8080 --host 0.0.0.0 -d _site
+```
+
+# Development Notes
+1. We use [Bulma](https://bulma.io) as our CSS Framework. It is a lightweight framework based on Flexbox and does not bring in any JS.
+
+2. To standardize file configuration (tabs vs. spaces, newlines at the end of files), we use [EditorConfig](https://editorconfig.org/)
+    - For a lot of text editors, you can install the EditorConfig extension and it will automatically configure any file you add or change
+    - It will do things like adding an empty line to the end of the file or trimming whitespace from the end of a line.
+
+3. We use Tiny Slider for our Highlight and News carousels: [Link to Github](https://github.com/ganlanyuan/tiny-slider)
+
+4. To track page views and event funnels, we use [Woopra](https://www.woopra.com).
+
+5. To process payments and webhooks, we have a Ruby on Rails API that is hosted on Heroku. Here is the link the [repo](https://github.com/jnewcs/hogar-nazaret-api)
+
+5. To actually deploy, we don't use the [pages-gem](https://github.com/github/pages-gem), we are using Github Actions following this [blog post](https://sujaykundu.com/blog/introducing-devlopr-easily-deploy-your-jekyll-blog-using-github-pages-and-github-actions/). This allows us to use Jekyll 4.2.0 which is a lot faster at building the website.
---
+++ js/main.js
@@ -0,0 +1,271 @@
+document.addEventListener('DOMContentLoaded', function () {
+  /* Code to lazy load script */
+  window.stripeLoaded = false;
+  window.lazyLoadScript = function(scriptSrc, postLoadFunction, failEvent) {
+    if (!scriptSrc) return;
+
+    var script = document.createElement('script');
+    script.src = scriptSrc;
+    script.defer = true;
+    script.async = false;
+    script.onload = function() {
+      if (postLoadFunction) {
+        postLoadFunction();
+      }
+    };
+    script.onerror = function() {
+      window.trackEvent(failEvent)
+    };
+    document.body.appendChild(script);
+  }
+
+  /* Navbar Burger Clicker */
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', function (){
+        var target = el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+
+  /* Navbar dropdown clicking on mobile */
+  var $navbarItemsWithDropdown = Array.prototype.slice.call(document.querySelectorAll('.navbar-item.has-dropdown'), 0);
+  if ($navbarItemsWithDropdown.length > 0) {
+    $navbarItemsWithDropdown.forEach(el => {
+      el.addEventListener('click', function () {
+        el.classList.toggle('is-open');
+      });
+    });
+  }
+
+  /*
+   * Code for Main Highlight Carousel
+   * We use Tiny Slider for our Carousels:
+   * https://github.com/ganlanyuan/tiny-slider
+   */
+  //
+  var carouselContainer = document.getElementById('carousel-container');
+  if (carouselContainer) {
+    var highlightCarousel = tns({
+      container: '#carousel-container',
+      items: 1,
+      slideBy: 'page',
+      preventScrollOnTouch: 'auto',
+      speed: 400,
+      edgePadding: 0,
+      mouseDrag: true,
+      autoHeight: true,
+      nav: false,
+      loop: false,
+      controls: true,
+      controlsContainer: document.getElementById('controls-container'),
+      lazyload: true,
+      arrowKeys: false,
+      onInit: function() {
+        var highlights = Array.prototype.slice.call(document.querySelectorAll('.carousel-highlight-container'), 0);
+        highlights.forEach(highlight => {
+          highlight.classList.remove('is-hidden');
+        });
+      }
+    });
+
+    // On mobile, we need to manually adjust the height
+    var indexChangedHandler = function (_info, _eventName) {
+      highlightCarousel.updateSliderHeight()
+    }
+    highlightCarousel.events.on('indexChanged', indexChangedHandler);
+
+    var autoplayButton = document.getElementById('autoplay-button');
+    if (highlightCarousel && autoplayButton) {
+      // Manually setup autoplay for main highlight carousel
+      var turnOffAutoPlay = function() {
+        clearInterval(interval);
+        autoplayButton.dataset.action = 'start';
+      };
+
+      var interval;
+      var setAutoPlay = function() {
+        interval = setInterval(function () {
+          highlightCarousel.goTo('next');
+
+          var info = highlightCarousel.getInfo();
+          if (info.slideCount === info.displayIndex) {
+            // This is the last slide, turn off autoplay
+            turnOffAutoPlay();
+          }
+        }, 7500);
+      };
+      setAutoPlay();
+
+      autoplayButton.addEventListener('click', function () {
+        var currentAction = autoplayButton.dataset.action;
+        if (currentAction === 'stop') {
+          turnOffAutoPlay();
+        } else {
+          setAutoPlay();
+          autoplayButton.dataset.action = 'stop';
+        }
+      });
+
+      document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 37) {
+          turnOffAutoPlay();
+          highlightCarousel.goTo('prev');  // left-arrow
+        } else if (event.keyCode === 39) {
+          turnOffAutoPlay();
+          highlightCarousel.goTo('next'); // right-arrow
+        }
+      });
+    }
+  }
+
+  /* Code for News Carousel */
+  var newsCarouselContainer = document.getElementById('news-carousel-container');
+  if (newsCarouselContainer) {
+    tns({
+      container: '#news-carousel-container',
+      items: 1,
+      responsive: {
+        900: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
+        }
+      },
+      edgePadding: 0,
+      slideBy: 1,
+      autoplay: false,
+      arrowKeys: false,
+      preventScrollOnTouch: 'auto',
+      speed: 400,
+      mouseDrag: true,
+      nav: false,
+      loop: true,
+      controls: false,
+      autoHeight: true,
+      autoplay: true,
+      autoplayTimeout: 15000,
+      autoplayText: ['', ''],
+      autoplayButtonOutput: false,
+      lazyload: true
+    });
+  }
+
+  /* Code for Lazy Loading */
+  var lazyImages = document.querySelectorAll('.lazy-load');
+  if (lazyImages && lazyImages.length) {
+    var observer = lozad(lazyImages);
+    observer.observe();
+  }
+
+  /* Code for modal opening */
+  var modalContainer = document.getElementById('donation-content-container');
+  var loadingContainer = document.getElementById('loading-container');
+  var successContainer = document.getElementById('success-container');
+  var errorContainer = document.getElementById('error-container');
+  var $modalOpeners = Array.prototype.slice.call(document.querySelectorAll('.modal-opener'), 0);
+  var setStripeLoaded = function() {
+    window.stripeLoaded = true;
+  };
+
+  if ($modalOpeners.length > 0) {
+    $modalOpeners.forEach(el => {
+      el.addEventListener('click', function () {
+        var target = el.dataset.target;
+        if (target === 'donation-modal') {
+          if (window.stripeLoaded === false) {
+            window.lazyLoadScript('https://js.stripe.com/v3', setStripeLoaded, 'async_stripe_load_fail');
+            var paymentModalSrc = document.getElementById('payment-modal-src').dataset.src;
+            window.lazyLoadScript(paymentModalSrc, undefined, 'async_payment_modal_fail');
+          }
+
+          // Track clicks to open the donation modal
+          window.trackEvent('donation_model_open');
+        }
+        var $target = document.getElementById(target);
+        modalContainer.classList.remove('is-hidden');
+        loadingContainer.classList.add('is-hidden');
+        successContainer.classList.add('is-hidden');
+        errorContainer.classList.add('is-hidden');
+
+        document.documentElement.classList.add('is-clipped');
+        $target.classList.add('is-active');
+      });
+    });
+  }
+
+  /* Code for modal closing */
+  var $modalClosers = Array.prototype.slice.call(document.querySelectorAll('.modal-closer'), 0);
+  if ($modalClosers.length > 0) {
+    $modalClosers.forEach(el => {
+      el.addEventListener('click', function () {
+        var target = el.dataset.target;
+        if (target === 'donation-modal') {
+          // Track clicks to close the donation modal
+          window.trackEvent('donation_model_close');
+        }
+        var $target = document.getElementById(target);
+
+        document.documentElement.classList.remove('is-clipped');
+        $target.classList.remove('is-active');
+      });
+    });
+  }
+
+  /* Code for tab switching */
+  var $tabs = Array.prototype.slice.call(document.querySelectorAll('.tab'), 0);
+  var $tabContents = Array.prototype.slice.call(document.querySelectorAll('.tab-content'), 0);
+  if ($tabs.length > 0) {
+    $tabs.forEach(el => {
+      el.addEventListener('click', function () {
+        var target = el.dataset.target;
+        if (['donate-now-content', 'sponsor-content'].indexOf(target) !== -1) {
+          // Track views on the different tabs
+          var donationType = (target === 'donate-now-content') ? 'one_time' : 'monthly_donation';
+          window.trackEvent('donation_model_view', {
+            donation_type: donationType
+          });
+        }
+        var $target = document.getElementById(target);
+
+        // Remove active from tabs and tab content
+        $tabs.forEach(innerTab => {
+          innerTab.classList.remove('is-active');
+        });
+        $tabContents.forEach(tabContent => {
+          tabContent.classList.remove('is-active');
+        });
+
+        el.classList.add('is-active');
+        $target.classList.add('is-active');
+      });
+    });
+  }
+
+  /* Code for lazy loading Youtube Videos */
+  var youtubeContainers = document.querySelectorAll('.youtube-player-container');
+  for (var i = 0; i < youtubeContainers.length; i++) {
+    var image = new Image();
+    image.alt = 'preview image';
+    image.src = 'https://img.youtube.com/vi/' + youtubeContainers[i].dataset.embed + '/sddefault.jpg';
+    image.addEventListener('load', function() {
+      youtubeContainers[i].appendChild(image);
+    }(i));
+
+    youtubeContainers[i].addEventListener('click', function() {
+      var iframe = document.createElement('iframe');
+      iframe.setAttribute('frameborder', '0');
+      iframe.setAttribute('allowfullscreen', '');
+      iframe.setAttribute('src', 'https://www.youtube.com/embed/' + this.dataset.embed + '?rel=0&showinfo=0&autoplay=1');
+      this.innerHTML = '';
+      this.appendChild(iframe);
+    });
+  }
+});
